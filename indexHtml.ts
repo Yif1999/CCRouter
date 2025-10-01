03c429d78d26091d26e6cab548d30784950d862c
yif <yiguanyaoyaofen@foxmail.com>
2025-10-01 11:42:27 +0800

    更改网站标题

--- indexHtml.ts
+++ indexHtml.ts
@@ -5,7 +5,7 @@ export const indexHtml = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>CCRouter - Apple Glass UI</title>
+<title>CCRouter</title>
 <link rel="shortcut icon" type="image/svg+xml" href="${faviconDataUrl}">
 <style>
     * { margin: 0; padding: 0; box-sizing: border-box; }
